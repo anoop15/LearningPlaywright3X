a9fde41d25f7132c9fc91c4f7787aeccedc69447
anoop15 <anoopsharma.qa@gmail.com>
2026-07-23 06:58:25 +0530

    Added switch chapter and user inputs chapter
    Co-authored-by: CommandCodeBot <noreply@commandcode.ai>

---
+++ 07_chapter_switch/43_Switch_Group.js
@@ -0,0 +1,18 @@
+let browser = "Brave";
+
+switch(browser) {
+    case "Chrome":
+    case "Eadge":
+    case "Brave":
+    case "Opera":
+        console.log("chromium project");
+        break;
+    case "Firefox":
+        console.log("mozilla project");
+        break;
+    case "Safari":
+        console.log("apple project - uses javascript engine");
+        break;
+    default:
+        console.log("unknown browser - menual testing method");
+}
